dae0d83260c65de6027fe6fa960ea79decf32713
Paul156551 <pavel.ryzhkov.99@mail.ru>
2022-10-25 21:55:43 +0300

    Связь почты с сайтом:https://pochtao.ru

---
+++ app/src/main/java/com/example/pochtao/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pochtao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://pochtao.ru/");
+        WebSettings websettings = webView.getSettings();
+        websettings.setJavaScriptEnabled(true);
+    }
+}
